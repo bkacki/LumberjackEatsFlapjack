d10bd49a7efa039c3b1223bd813a08bc2f237c3c
Bartek <bartlomiej.kacki@gmail.com>
2024-07-08 19:39:11 +0200

    Add project files.

---
+++ LumberjackEatsFlapjack/Lumberjack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LumberjackEatsFlapjack
+{
+    internal class Lumberjack
+    {
+        public string Name { get; private set; }
+        private Stack<Flapjack> flapjackStack = new Stack<Flapjack>();
+        private static readonly Random random = new Random();
+
+        public Lumberjack(string name)
+        {
+            Name = name;
+        }
+
+        public void TakeFlapjack()
+        {
+            flapjackStack.Push((Flapjack)random.Next(4));
+        }
+
+        public void EatFlapjack()
+        {
+            Console.WriteLine($"{Name} je naleśniki.");
+            while (flapjackStack.Count > 0)
+                Console.WriteLine($"{Name} je {flapjackStack.Pop()} naleśnik.");
+        }
+    }
+}
---
+++ LumberjackEatsFlapjack/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace LumberjackEatsFlapjack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Lumberjack> lumberjackQueue = new Queue<Lumberjack>();
+            Console.Write("Imię pierwszego drwala: ");
+            lumberjackQueue.Enqueue(new Lumberjack(Console.ReadLine()));
+            Console.Write("Libcza naleśników: ");
+            if (int.TryParse(Console.ReadLine(), out int numberOfFlapjack))
+                for (int i = 0; i < numberOfFlapjack; i++)
+                    lumberjackQueue.Peek().TakeFlapjack();
+            string lumberjackName = "";
+            do
+            {
+                Console.Write("Imię następnego drwala (pusty wiersz aby zakończyć): ");
+                lumberjackName = Console.ReadLine();
+                if (lumberjackName != "")
+                {
+                    lumberjackQueue.Enqueue(new Lumberjack(lumberjackName));
+                    Console.Write("Liczba naleśników: ");
+                    Stack<Lumberjack> lumberjackStack = new Stack<Lumberjack>(lumberjackQueue);
+                    if (int.TryParse(Console.ReadLine(), out numberOfFlapjack))
+                        for (int i = 0; i < numberOfFlapjack; i++)
+                            lumberjackStack.Peek().TakeFlapjack();
+                }
+            } while (lumberjackName != "");
+
+            while (lumberjackQueue.Count > 0)
+                lumberjackQueue.Dequeue().EatFlapjack();
+        }
+    }
+}
